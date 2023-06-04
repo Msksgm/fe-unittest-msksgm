f4b83d518bee5ab5b7edae896f2aa9bb2af5a934
Msksgm <msksgm3124@gmail.com>
2023-06-04 15:12:22 +0900

    test: pp.82--pp.85 リスト4-33 ~ リスト4-38

--- src/04/fetchers/index.ts
+++ src/04/fetchers/index.ts
@@ -1,4 +1,4 @@
-import type { Articles, Profile } from "./type";
+import type { Article, ArticleInput, Articles, Profile } from "./type";
 
 export const getMyProfile = (): Promise<Profile> => {
   return fetch("https://myapi.testing.com/my/profile").then(async (res) => {
@@ -12,6 +12,19 @@ export const getMyProfile = (): Promise<Profile> => {
 
 export const getMyArticles = (): Promise<Articles> => {
   return fetch("https://myapi.testing.com/my/articles").then(async (res) => {
+    const data = await res.json();
+    if (!res.ok) {
+      throw data;
+    }
+    return data;
+  });
+};
+
+export const postMyArticle = (input: ArticleInput): Promise<Article> => {
+  return fetch("https://myapi.testing.com//my/articles", {
+    method: "POST",
+    body: JSON.stringify(input),
+  }).then(async (res) => {
     const data = await res.json();
     if (!res.ok) {
       throw data;
